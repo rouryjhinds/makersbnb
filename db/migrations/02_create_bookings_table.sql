5ce95122ff4741b24f7e9f83dd21d5944234866b
Sarah <sarahchosen@hotmail.co.uk>
2021-01-21 11:23:30 +0000

    updated databases

--- db/migrations/02_create_bookings_table.sql
+++ db/migrations/02_create_bookings_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bookings (id SERIAL PRIMARY KEY, start_date DATE, end_date DATE, spaces_id INTEGER, FOREIGN KEY (spaces_id) REFERENCES spaces(id));
+CREATE TABLE bookings (id SERIAL PRIMARY KEY, start_date DATE, end_date DATE, spaces_id INTEGER, FOREIGN KEY (spaces_id) REFERENCES spaces(id), requested boolean, confirmed, boolean);
